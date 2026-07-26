0097f6c647ebdc289da3eed4c7f36683dd47917f
Safiyo Mohamed <Smohamed166@student.cscc.edu>
2026-07-26 19:54:44 -0400

    p-3 split settings into dynamic and static, added increase difficulty function

--- settings.py
+++ settings.py
@@ -8,28 +8,26 @@ class Settings:
         self.screen_h = 800
         self.FPS = 60
         self.bg_file = Path.cwd()/ 'Assets'/ 'images' / 'Starbasesnow.png'
+        self.difficulty_scale = 1.1
 
         self.ship_file = Path.cwd() / 'Assets' / 'Images' / 'ship2(no bg).png'
         self.ship_w = 110      
         self.ship_h = 80
-        self.ship_speed = 5
-        self.starting_ship_count = 3 
+        
 
         self.bullet_file = Path.cwd()/ 'Assets'/ 'images' / 'laserBlast.png'
         self.laser_sound = Path.cwd()/ 'Assets'/ 'sound' / 'laser.mp3'
         self.impact_sound = Path.cwd()/ 'Assets'/ 'sound' / 'impactSound.mp3'
 
-        self.bullet_speed = 7
-        self.bullet_w = 25
-        self.bullet_h = 80
-        self.bullet_amount = 5
+       
+        
+        
 
         self.alien_file = Path.cwd()/ 'Assets'/ 'images' / 'enemy_4.png'
         self.alien_w = 50
         self.alien_h = 80
-        self.fleet_speed = 5
         self.fleet_direction = 1 
-        self.fleet_drop_speed = 40
+        
 
 
         self.button_w = 200
@@ -40,3 +38,23 @@ class Settings:
         self.button_font_size = 48
         self.HUD_font_size = 20
         self.font_file = Path.cwd() /'assets' / 'Fonts' / 'Silkscreen' / 'silkscreen-Bold.ttf' 
+
+
+
+        def  initialize_dynamic_settings(self):
+            self.bullet_w = 25
+            self.bullet_h = 80
+            self.bullet_speed = 7
+            self.bullet_amount = 5
+
+            self.starting_ship_count = 3 
+            self.ship_speed = 5
+           
+            self.fleet_speed = 5
+            self.fleet_drop_speed = 40
+
+        def increase_difficulty(self):
+            self.ship_speed *= self.difficulty_scale
+            self.bullet_speed *= self.difficulty_scale
+            self.fleet_speed *= self.difficulty_scale
+            
