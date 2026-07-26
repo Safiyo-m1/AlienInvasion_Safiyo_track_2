d39dd20a3d43e7040e119d85af318738bce9067f
Safiyo Mohamed <Smohamed166@student.cscc.edu>
2026-07-26 19:34:32 -0400

    p-3Set up play button to start game and make mouse disappear

--- alien_invasion.py
+++ alien_invasion.py
@@ -7,6 +7,7 @@ from arsenal import Arsenal
 from alien_fleet import AlienFleet
 from game_stats import GameStats
 from time import sleep
+from button import Button
 
 class AlienInvasion:
 
@@ -40,7 +41,9 @@ class AlienInvasion:
         self.ship = Ship(self, Arsenal(self))
         self.alien_fleet = AlienFleet(self) 
         self.alien_fleet.create_fleet() 
-        self.game_active = True
+        self.play_button = Button(self,'Play') 
+        self.game_active = False
+    
 
 
 
@@ -96,10 +99,26 @@ class AlienInvasion:
         self.alien_fleet.create_fleet()
 
 
+    def restart_game(self):
+        #setting up dynamic settings
+        #reset Game stats
+        #update HUD scores
+        
+
+        self._reset_level()
+        self.ship._center_ship()
+        self.game_active = True
+        pygame.mouse.set_visible(False)
+
+
     def _update_screen(self):
         self.screen.blit(self.bg, (0,0))
         self.ship.draw()
         self.alien_fleet.draw()
+        if not self.game_active: 
+            self.play_button.draw()
+            pygame.mouse.set_visible(True)
+    
         pygame.display.flip()
 
     def _check_events(self):
@@ -114,6 +133,13 @@ class AlienInvasion:
 
             elif event.type == pygame.KEYUP:
                 self._check_keyup_events(event)
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                self._check_button_clicked()
+
+    def _check_button_clicked(self):
+        mouse_pos =pygame.mouse.get_pos()
+        if self.play_button.check_clicked(mouse_pos):
+            self.restart_game() 
 
 
     def _check_keyup_events(self, event):
--- settings.py
+++ settings.py
@@ -30,3 +30,13 @@ class Settings:
         self.fleet_speed = 5
         self.fleet_direction = 1 
         self.fleet_drop_speed = 40
+
+
+        self.button_w = 200
+        self.button_h = 60
+        self.button_color = (0,135,50)
+
+        self.text_color = (225, 255, 255)
+        self.button_font_size = 48
+        self.HUD_font_size = 20
+        self.font_file = Path.cwd() /'assets' / 'Fonts' / 'Silkscreen' / 'silkscreen-Bold.ttf' 
